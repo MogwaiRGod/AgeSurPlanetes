93f2a08f8575388945eca7eb59aaab269095ae4c
MogwaiRGod <diane.roberg@gmail.com>
2023-01-17 16:41:22 +0100

    fin structure HTML + début style

--- periode_orbitale.js
+++ periode_orbitale.js
@@ -9,10 +9,11 @@ const tab_planetes={
     'Uranus':30666.14879,
     'Neptune':60148.8318
 };
+let resultat = document.getElementsByTagName("resultat");
 
 /// inputs
 let age=parseInt(window.prompt("Entrer votre âge :")); // conversion en entier de l'entrée utilisateur
-let planete;
+let planete="Vénus";
 
 
 
@@ -22,15 +23,15 @@ const verifInput = (age) => { return age != null && !isNaN(age); }; // vérifie 
 /// calculs
 const convertirAgeEnJours = (age) => { return age*=365; };
 // calcule l'âge équivalent selon la planète entrée et l'arrondit à l'entier le plus proche
-const convertirAgePlanete = (age, planete) => { return Math.round(age/tab_planetes[planete]); }; // planete = clef
-const afficherAge = (age, planete) => { document.write(`Votre âge sur ${planete} est : ${age} ans.<br>`); };
+const convertirAgePlanete = (age, planete) => { return Math.round(age/tab_planetes[planete]);}; // planete = clef
+const afficherAge = (age, planete) => { resultat=(`Votre âge sur ${planete} est : ${age} ans.<br>`); return resultat;};
 
 
 
 /////////// SCRIPT
 if (verifInput(age)) {
-    age=convertirAgePlanete(convertirAgeEnJours(age), clef);
-    afficherAge(age, clef);
+    age=convertirAgePlanete(convertirAgeEnJours(age), planete);
+    afficherAge(age, planete);
 } else {
     window.alert("Valeur incorrecte");
 }
